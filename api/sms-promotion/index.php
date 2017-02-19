35afb4f4d6bfbb4b803c85e0cd7eb426c28d4522
arianahk <arianahk@users.noreply.github.com>
2017-02-19 23:11:24 +0200

    smsverification

---
+++ api/sms-promotion/index.php
@@ -0,0 +1,71 @@
+<?php 
+//if (!isset($HTTP_RAW_POST_DATA)) { header('location: https://www.lamdacs.com/');}
+header('Content-Type: text/html; charset=utf-8');
+header('Cache-Control: no-store, no-cache, must-revalidate'); 
+
+require __DIR__ .'\twilio-php-master\Twilio\autoload.php';
+use Twilio\Rest\Client;
+ include("../../config.php");
+
+		$postdata = file_get_contents("php://input"); 
+		parse_str($postdata);
+		$errorcode=0;
+ 
+       if(!isset($api_key) || empty($api_key) || $api_key!='APIKEY')
+	   { 
+			header("HTTP/1.1 204 No Content");
+			die();
+		 
+	   }else 
+	   { 
+	    
+       if($errorcode==0)
+            {	
+					
+					if(!isset($phone) || !is_numeric($phone))
+					{
+						$errorcode=2; 
+					}
+					 
+					 if($errorcode==0)
+					 {
+						   
+							if(date('a') == 'am'){
+								$msg= 'Good morning! Your promocode is AM123';
+							}else{
+								$msg= 'Hello! Your promocode is PM456';
+							}
+
+							$client = new Client($account_sid, $auth_token);
+							try{
+							$rescode=$client->messages->create("+".$phone,
+												array('from' => $fromnumber,
+													  'body' => $msg)
+											  );
+						 
+							$sid=$rescode->sid; 				
+ 							$body=$rescode->body;
+							if (isset($sid))
+							{ 		
+								$response['sid']=$sid;
+								$response['body']=$body;
+							}
+ 
+ 
+							}catch(Exception $e) {
+								 
+									$response['error_message']=$e->getMessage();
+									 
+								}
+ 
+					 }
+     
+             } 
+       }
+	  
+
+ 
+			$postdata = json_encode($response);
+			echo trim($postdata);
+	   
+?>
---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+
+/** Test Credentials : Replace below codes with production codes*/
+$account_sid ="AC77b20a871d449085a33185a92f32abd7";
+$auth_token = "2c3ac54e2843e0fb95558d47dd75bcf5";
+
+
+/*Test valid from number */
+$fromnumber="+15005550006";
+
+/*API URL*/
+$apiurl="http://127.0.0.1/bdswiss/api/sms-promotion/index.php";
+
+?>
